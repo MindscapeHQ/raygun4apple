65b1da3057e8344b213a9c6923ac08e60122f066
Mitchell Duncan <mitchell@raygun.com>
2018-07-26 16:28:49 +1200

    Adding the new Xcode project with the KSCrash SDK.

---
+++ raygun4apple tvOS/raygun4apple_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  raygun4apple_tvOS.h
+//  raygun4apple tvOS
+//
+//  Created by Mitchell Duncan on 18/07/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for raygun4apple_tvOS.
+FOUNDATION_EXPORT double raygun4apple_tvOSVersionNumber;
+
+//! Project version string for raygun4apple_tvOS.
+FOUNDATION_EXPORT const unsigned char raygun4apple_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <raygun4apple_tvOS/PublicHeader.h>
+
+
