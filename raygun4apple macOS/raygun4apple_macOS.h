7512061e924149c038c36b685ed6a98d614f578e
raygundev <raygundev@Mindscapes-Macbook-Pro-2.local>
2018-07-30 11:25:03 +1200

    Add macOS project

---
+++ raygun4apple macOS/raygun4apple_macOS.h
@@ -0,0 +1,18 @@
+//
+//  raygun4apple_macOS.h
+//  raygun4apple macOS
+//
+//  Created by raygundev on 7/30/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for raygun4apple_macOS.
+FOUNDATION_EXPORT double raygun4apple_macOSVersionNumber;
+
+//! Project version string for raygun4apple_macOS.
+FOUNDATION_EXPORT const unsigned char raygun4apple_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <raygun4apple_macOS/PublicHeader.h>
+
+
