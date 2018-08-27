132e09e5165b8149d0bb3d1641082445490c05bd
Mitchell Duncan <mitchell@raygun.com>
2018-08-27 17:01:21 +1200

    Updating the license information.

--- src/Raygun/RaygunEnvironmentMessage.m
+++ src/Raygun/RaygunEnvironmentMessage.m
@@ -5,6 +5,16 @@
 //  Created by Mitchell Duncan on 11/09/17.
 //  Copyright © 2018 Raygun Limited. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
