508b630ed20b9b0a5b011ec559417a93fbb18d31
Mitchell Duncan <mitchell@raygun.com>
2018-09-14 14:02:28 +1200

    Converted c function to an objective c function and moved it into the logger class to avoid the compiler warning for unused function.

--- src/Raygun/RaygunDefines.h
+++ src/Raygun/RaygunDefines.h
@@ -116,17 +116,6 @@ typedef NS_ENUM(NSInteger, RaygunResponseStatusCode) {
     kRaygunResponseStatusCodeRateLimited   = 429,
 };
 
-static NSString* _Nonnull RaygunResponseStatusCodeMessage(NSInteger statusCode) {
-    switch ((RaygunResponseStatusCode)statusCode) {
-        case kRaygunResponseStatusCodeAccepted:      return @"Request succeeded"; break;
-        case kRaygunResponseStatusCodeBadMessage:    return @"Bad message - could not parse the provided JSON. Check all fields are present, especially both occurredOn (ISO 8601 DateTime) and details { } at the top level"; break;
-        case kRaygunResponseStatusCodeInvalidApiKey: return @"Invalid API Key - The value specified in the header X-ApiKey did not match with an application in Raygun"; break;
-        case kRaygunResponseStatusCodeLargePayload:  return @"Request entity too large - The maximum size of a JSON payload is 128KB"; break;
-        case kRaygunResponseStatusCodeRateLimited:   return @"Too Many Requests - Plan limit exceeded for month or plan expired"; break;
-        default: [NSString stringWithFormat:@"Response status code: %ld",(long)statusCode]; break;
-    }
-}
-
 static inline BOOL IsNullOrEmpty(id _Nullable thing) {
     return thing == nil || ([thing respondsToSelector:@selector(length)] && ((NSData *)thing).length == 0)
                         || ([thing respondsToSelector:@selector(count)] && ((NSArray *)thing).count == 0);
--- src/Raygun/RaygunLogger.m
+++ src/Raygun/RaygunLogger.m
@@ -39,15 +39,28 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 + (void)logResponseStatusCode:(NSInteger)statusCode {
+    NSString *message = [self ResponseStatusCodeMessage:statusCode];
+    
     switch ((RaygunResponseStatusCode)statusCode) {
-        case kRaygunResponseStatusCodeAccepted: [self logDebug:RaygunResponseStatusCodeMessage(statusCode)]; break;
+        case kRaygunResponseStatusCodeAccepted: [self logDebug:message]; break;
             
         case kRaygunResponseStatusCodeBadMessage:    // fall through
         case kRaygunResponseStatusCodeInvalidApiKey: // fall through
         case kRaygunResponseStatusCodeLargePayload:  // fall through
-        case kRaygunResponseStatusCodeRateLimited: [self logError:RaygunResponseStatusCodeMessage(statusCode)]; break;
+        case kRaygunResponseStatusCodeRateLimited: [self logError:message]; break;
             
-        default: [self logDebug:RaygunResponseStatusCodeMessage(statusCode)]; break;
+        default: [self logDebug:message]; break;
+    }
+}
+
++  (NSString * _Nonnull)ResponseStatusCodeMessage:(NSInteger)statusCode {
+    switch ((RaygunResponseStatusCode)statusCode) {
+        case kRaygunResponseStatusCodeAccepted:      return @"Request succeeded"; break;
+        case kRaygunResponseStatusCodeBadMessage:    return @"Bad message - could not parse the provided JSON. Check all fields are present, especially both occurredOn (ISO 8601 DateTime) and details { } at the top level"; break;
+        case kRaygunResponseStatusCodeInvalidApiKey: return @"Invalid API Key - The value specified in the header X-ApiKey did not match with an application in Raygun"; break;
+        case kRaygunResponseStatusCodeLargePayload:  return @"Request entity too large - The maximum size of a JSON payload is 128KB"; break;
+        case kRaygunResponseStatusCodeRateLimited:   return @"Too Many Requests - Plan limit exceeded for month or plan expired"; break;
+        default: [NSString stringWithFormat:@"Response status code: %ld",(long)statusCode]; break;
     }
 }
 
