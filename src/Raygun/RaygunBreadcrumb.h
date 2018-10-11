857180d088b685179b2e391600e6a53cb923c214
Mitchell Duncan <mitchell@raygun.com>
2018-10-12 10:28:06 +1300

    Add block initialization method.

--- src/Raygun/RaygunBreadcrumb.h
+++ src/Raygun/RaygunBreadcrumb.h
@@ -17,18 +17,60 @@ typedef void(^RaygunBreadcrumbBlock)(RaygunBreadcrumb *breadcrumb);
 
 @interface RaygunBreadcrumb : NSObject
 
+/**
+ The message you want to record for this breadcrumb
+ */
 @property (nonatomic, copy) NSString *message;
+
+/**
+ Any value to categorize your messages
+ */
 @property (nonatomic, copy) NSString *category;
+
+/**
+ The display level of the message (valid values are Debug, Info, Warning, Error)
+ */
 @property (nonatomic) enum  RaygunBreadcrumbLevel level;
+
+/**
+ The type of message (valid values are manual only currently)
+ */
 @property (nonatomic) enum  RaygunBreadcrumbType type;
+
+/**
+ Milliseconds since the Unix Epoch (required)
+ */
 @property (nonatomic, copy) NSNumber *occurredOn;
+
+/**
+ If relevant, a class name from where the breadcrumb was recorded
+ */
 @property (nonatomic, copy) NSString *className;
+
+/**
+ If relevant, a method name from where the breadcrumb was recorded
+ */
 @property (nonatomic, copy) NSString *methodName;
+
+/**
+ If relevant, a line number from where the breadcrumb was recorded
+ */
 @property (nonatomic, copy) NSNumber *lineNumber;
+
+/**
+ Any custom data you want to record about application state when the breadcrumb was recorded
+ */
 @property (nonatomic, strong) NSDictionary *customData;
 
 + (instancetype)breadcrumbWithBlock:(RaygunBreadcrumbBlock)block;
+
 - (instancetype)initWithBlock:(RaygunBreadcrumbBlock)block NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)breadcrumbWithInformation:(NSDictionary *)information;
+
++ (BOOL)validate:(RaygunBreadcrumb *)breadcrumb withError:(NSError **)error;
 
 - (NSDictionary *)convertToDictionary;
 
--- src/Raygun/RaygunBreadcrumb.m
+++ src/Raygun/RaygunBreadcrumb.m
@@ -8,6 +8,7 @@
 #import "RaygunBreadcrumb.h"
 
 #import "RaygunUtils.h"
+#import "NSError+SimpleConstructor.h"
 
 @implementation RaygunBreadcrumb
 
@@ -23,10 +24,70 @@
     NSParameterAssert(block);
     self = [super init];
     if (self) {
+        // Set default values
+        _occurredOn = [RaygunUtils timeSinceEpochInMilliseconds];
+        _type = kRaygunBreadcrumbTypeManual;
+        
         block(self);
     }
     
     return self;
+}
+
++ (instancetype)breadcrumbWithInformation:(NSDictionary *)information {
+    RaygunBreadcrumb *breadcrumb = [RaygunBreadcrumb breadcrumbWithBlock:^(RaygunBreadcrumb *breadcrumb) {
+        breadcrumb.message    = information[@"message"];
+        breadcrumb.category   = information[@"category"];
+        breadcrumb.level      = [RaygunBreadcrumb levelEnumFromString:information[@"level"]];
+        breadcrumb.type       = [RaygunBreadcrumb typeEnumFromString:information[@"type"]];
+        breadcrumb.occurredOn = information[@"timeStamp"];
+        breadcrumb.className  = information[@"className"];
+        breadcrumb.methodName = information[@"methodName"];
+        breadcrumb.lineNumber = information[@"lineNumber"];
+        breadcrumb.customData = information[@"customData"];
+    }];
+    return breadcrumb;
+}
+
++ (RaygunBreadcrumbLevel)levelEnumFromString:(NSString *)level {
+    static NSDictionary *levelDict = nil;
+    if (levelDict == nil) {
+        levelDict = @{ RaygunBreadcrumbLevelNames[kRaygunBreadcrumbLevelDebug]:   [[NSNumber alloc] initWithInt:kRaygunBreadcrumbLevelDebug],
+                       RaygunBreadcrumbLevelNames[kRaygunBreadcrumbLevelInfo]:    [[NSNumber alloc] initWithInt:kRaygunBreadcrumbLevelInfo],
+                       RaygunBreadcrumbLevelNames[kRaygunBreadcrumbLevelWarning]: [[NSNumber alloc] initWithInt:kRaygunBreadcrumbLevelWarning],
+                       RaygunBreadcrumbLevelNames[kRaygunBreadcrumbLevelError]:   [[NSNumber alloc] initWithInt:kRaygunBreadcrumbLevelError] };
+    }
+    
+    return [levelDict[level] intValue];
+}
+
++ (RaygunBreadcrumbType)typeEnumFromString:(NSString *)type {
+    static NSDictionary *typeDict = nil;
+    if (typeDict == nil) {
+        typeDict = @{ RaygunBreadcrumbTypeNames[kRaygunBreadcrumbTypeManual]: [[NSNumber alloc] initWithInt:kRaygunBreadcrumbTypeManual] };
+    }
+    
+    return [typeDict[type] intValue];
+}
+
++ (BOOL)validate:(RaygunBreadcrumb *)breadcrumb withError:(NSError* __autoreleasing *)error {
+    if (breadcrumb == nil) {
+        [NSError fillError:error
+                withDomain:[[self class] description]
+                      code:0
+               description:@"Breacrumb cannot be nil"];
+        return NO;
+    }
+    
+    if (breadcrumb.occurredOn == nil) {
+        [NSError fillError:error
+                withDomain:[[self class] description]
+                      code:0
+               description:@"Breacrumb must have a valid occurred on time stamp"];
+        return NO;
+    }
+    
+    return YES;
 }
 
 - (NSDictionary *)convertToDictionary {
@@ -35,7 +96,7 @@
     dict[@"message"]   = _message;
     dict[@"level"]     = RaygunBreadcrumbLevelNames[_level];
     dict[@"type"]      = RaygunBreadcrumbTypeNames[_type];
-    dict[@"timeStamp"] = _occurredOn;
+    dict[@"timeStamp"] = @([_occurredOn longValue]);
     
     if (![RaygunUtils IsNullOrEmpty:_category]) {
         dict[@"category"] = _category;
