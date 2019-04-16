5adb9e04ace8e570923f7efb37d904943aaa662c
Mitchell Duncan <mitchell@raygun.com>
2019-04-16 15:52:49 +1200

    Ensure the headers are public and available.

--- crash iOS/crash_iOS.h
+++ crash iOS/crash_iOS.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char crash_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <crash_iOS/PublicHeader.h>
 
-
+#import "Crash.h"
